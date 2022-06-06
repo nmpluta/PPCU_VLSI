d90008d83e1b2eb27e45d47cfa1ad6688d55182a
Natalia Pluta <natalia0pluta@gmail.com>
2022-06-06 22:22:22 +0200

    #1 morse code in led

--- sw/app/src/main.cpp
+++ sw/app/src/main.cpp
@@ -1,31 +1,67 @@
 #include "delay.h"
 #include "gpio.h"
 
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
 static constexpr uint32_t led_mask{0xF};
 
-
 // SOS: ... --- ...
-static constexpr bool sos_morse[] = { 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1 };
+static constexpr bool sos_morse[] = {1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1};
 
-// Natalia:  
-static constexpr bool name_morse[] = { 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1 };
+// Natalia:
+static constexpr bool name_morse[] = {1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1};
 
 // Pluta:
 static constexpr bool surname_morse[] = {1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1};
 
 int main()
 {
-    while (true) 
+    while (true)
     {
-        for (int i = 0; i < 16; ++i) 
-        {
-            gpio.set_mask_bits(led_mask, i);
-            // gpio.set_pin(0, true);
+        uint8_t length_sos = sizeof(sos_morse) / sizeof(sos_morse[0]);
+        uint8_t length_name = sizeof(name_morse) / sizeof(name_morse[0]);
+        uint8_t length_surname = sizeof(surname_morse) / sizeof(surname_morse[0]);
 
-            // gpio.set_pin(1, 1);
-            // gpio.set_pin(2, 0);
-            // gpio.set_pin(3, 1);
+        uint8_t max_length = MAX(MAX(length_sos, length_name), length_surname);
+
+        for (int i = 0; i < max_length; ++i)
+        {
+            gpio.set_pin(0, true);
+
+            if (i < length_sos)
+            {
+                gpio.set_pin(sos_morse[i], 1);
+            }
+            else
+            {
+                gpio.set_pin(1, false);
+            }
+
+            if (i < length_name)
+            {
+                gpio.set_pin(name_morse[i], 2);
+            }
+            else
+            {
+                gpio.set_pin(2, false);
+            }
+
+            if (i < length_surname)
+            {
+                gpio.set_pin(surname_morse[i], 3);
+            }
+            else
+            {
+                gpio.set_pin(3, false);
+            }
+
             mdelay(1000);
         }
+
+        gpio.set_pin(0, false);
+        gpio.set_pin(1, false);
+        gpio.set_pin(2, false);
+        gpio.set_pin(3, false);
+        mdelay(2000);
     }
 }
